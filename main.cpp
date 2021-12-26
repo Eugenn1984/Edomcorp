66a535d3b17164287590d785a736f979f3c71001
Eugeniy <simple.coding.Eugenn1984@gmail.com>
2021-12-26 15:38:10 +0200

    added second and thirde line

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 int main()
 {
-	std::cout << "Hello World" std::endl;
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Hi its mi" << std::endl;
+	std::cout << "Fusio" << std::endl;
 	return 0;
 }
